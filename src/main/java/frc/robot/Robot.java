7c1dd16bb847daee442defb2ac65ad994888c71f
austinfifield <42851763+austinfifield@users.noreply.github.com>
2020-12-10 22:08:46 -0500

    Removed default comments that cluttered code.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,25 +14,14 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.DriveForDistanceCommand;
 import frc.robot.subsystems.DriveTrainSubsystem;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * methods corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot
 {
     private Command autonomousCommand;
-
     private RobotContainer robotContainer;
     private DriveTrainSubsystem driveTrainSubsystem;
+    
     Constants constants = new Constants();
 
-    /**
-     * This method is run when the robot is first started up and should be used for any
-     * initialization code.
-     */
     @Override
     public void robotInit()
     {
@@ -41,28 +30,15 @@ public class Robot extends TimedRobot
         autonomousCommand = new DriveForDistanceCommand(driveTrainSubsystem, constants.inchesToDriveForDriveForDistanceCommand1);
     }
 
-    /**
-     * This method is called every robot packet, no matter the mode. Use this for items like
-     * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-     *
-     * <p>This runs after the mode specific periodic functions, but before
-     * LiveWindow and SmartDashboard integrated updating.
-     */
     @Override
     public void robotPeriodic()
     {
         SmartDashboard.putNumber("Left Encoder Distance" , driveTrainSubsystem.leftEncoder.getDistance());
         SmartDashboard.putNumber("Right Encoder Distance" , driveTrainSubsystem.rightEncoder.getDistance());
-        // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-        // commands, running already-scheduled commands, removing finished or interrupted commands,
-        // and running subsystem periodic() methods.  This must be called from the robot's periodic
-        // block in order for anything in the Command-based framework to work.
+
         CommandScheduler.getInstance().run();
     }
-
-    /**
-     * This method is called once each time the robot enters Disabled mode.
-     */
+  
     @Override
     public void disabledInit()
     {
@@ -73,22 +49,15 @@ public class Robot extends TimedRobot
     {
     }
 
-    /*
-     * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
-     */
     @Override
     public void autonomousInit()
     {
-        // schedule the autonomous command (example)
         if (autonomousCommand != null)
         {
             autonomousCommand.schedule();
         }
     }
 
-    /**
-     * This method is called periodically during autonomous.
-     */
     @Override
     public void autonomousPeriodic()
     {
@@ -98,19 +67,12 @@ public class Robot extends TimedRobot
     @Override
     public void teleopInit()
     {
-        // This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
         if (autonomousCommand != null)
         {
             autonomousCommand.cancel();
         }
     }
 
-    /**
-     * This method is called periodically during operator control.
-     */
     @Override
     public void teleopPeriodic()
     {
@@ -119,13 +81,9 @@ public class Robot extends TimedRobot
     @Override
     public void testInit()
     {
-        // Cancels all running commands at the start of test mode.
         CommandScheduler.getInstance().cancelAll();
     }
 
-    /**
-     * This method is called periodically during test mode.
-     */
     @Override
     public void testPeriodic()
     {
